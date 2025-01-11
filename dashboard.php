cf833c93e230121fe9abdf377f012bdefb5d3977
Yuyud727 <yudhistira2811@gmail.com>
2025-01-11 19:07:07 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -14,32 +14,38 @@ $jumlah_article = $hasil1->num_rows;
 //$jumlah_gallery = $hasil2->num_rows;
 ?>
 <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center pt-4">
+
     <div class="col">
-        <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
-            <div class="card-body">
-                <div class="d-flex justify-content-between">
-                    <div class="p-3">
-                        <h5 class="card-title"><i class="bi bi-newspaper"></i> Article</h5> 
+        <a href="/admin.php?page=article" class="text-decoration-none">
+            <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="p-3">
+                            <h5 class="card-title"><i class="bi bi-newspaper"></i> Article</h5> 
+                        </div>
+                        <div class="p-3">
+                            <span class="badge rounded-pill text-bg-danger fs-2"><?php echo $jumlah_article; ?></span>
+                        </div> 
                     </div>
-                    <div class="p-3">
-                        <span class="badge rounded-pill text-bg-danger fs-2"><?php echo $jumlah_article; ?></span>
-                    </div> 
                 </div>
             </div>
-        </div>
+        </a>
     </div> 
     <div class="col">
-        <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
-            <div class="card-body">
-                <div class="d-flex justify-content-between">
-                    <div class="p-3">
-                        <h5 class="card-title"><i class="bi bi-camera"></i> Gallery</h5> 
+        <a href="/admin.php?page=gallery" class="text-decoration-none">
+            <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="p-3">
+                            <h5 class="card-title"><i class="bi bi-camera"></i> Gallery</h5> 
+                        </div>
+                        <div class="p-3">
+                            <span class="badge rounded-pill text-bg-danger fs-2"><?php echo $jumlah_gallery; ?></span>
+                        </div> 
                     </div>
-                    <div class="p-3">
-                        <span class="badge rounded-pill text-bg-danger fs-2"><?php //echo $jumlah_gallery; ?></span>
-                    </div> 
                 </div>
             </div>
-        </div>
+        </a>    
     </div> 
+
 </div>
